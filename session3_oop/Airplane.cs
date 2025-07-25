304bec209d61d6409873b6ac0d7b9046f2547d4a
Yousef754 <yosefesam987@gmail.com>
2025-07-25 15:45:59 +0300

    V4

---
+++ session3_oop/Airplane.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session3_oop
+{
+    internal class Airplane : IMoveable, IFlyable
+    {
+        public int speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ session3_oop/IMoveable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session3_oop
+{
+    internal interface IMoveable
+    {
+     int speed {  get; set; }
+
+        void Forward()
+        {
+            Console.WriteLine("can imoveable forward");
+        }
+        void Backward()
+        {
+            Console.WriteLine("can imoveable Backward");
+        }
+        void left()
+        {
+            Console.WriteLine("can imoveable left");
+
+        }
+        void right()
+        {
+            Console.WriteLine("can imoveable right");
+        }
+
+
+
+
+    }
+}
