c51deee70b8f32999d7bf7a415dc012c13e2720f
Yousef754 <yosefesam987@gmail.com>
2025-07-25 14:22:00 +0300

    Add project files.

---
+++ session3_oop/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace session3_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region interfaces
+            // interfaces:reference type
+            //code contract between the developer who write it and the developer who use it
+            #endregion
+
+        }
+    }
+}
